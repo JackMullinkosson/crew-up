5472d182a22ad43c7c8586acb1bc39be896d4af8
Jack Mullinkosson <jack.mullinkosson@gmail.com>
2023-02-11 16:00:53 +0700

    can edit person in local and backedn

---
+++ pages/api/editPerson.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../prisma/client'
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try{
+        const {name, email, phoneNumber, roleName, order, id} = req.body
+        await prisma.person.update({
+            where: {
+                id: id
+            },
+            data: {
+                name: name,
+                email: email,
+                phoneNumber: phoneNumber,
+                roleName: roleName,
+                order: order
+            }
+          });
+          console.log(`Person with id ${id} was successfully edited.`);
+        } catch (error) {
+          console.error(`An error occurred while trying to edit person ${error}`);
+        }
+      }
