0fc174f06f3b0a485663a4731b409afbebb93b31
Jack Mullinkosson <jack.mullinkosson@gmail.com>
2023-02-20 14:23:43 +0700

    added transaction function to prisma

--- pages/api/reorderPeople.ts
+++ pages/api/reorderPeople.ts
@@ -7,24 +7,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       return res.status(405).end(); // Method Not Allowed
     }
     try {
-      const {people}  = req.body;
-      let updatedPeople = []
-      for (const [index, person] of people.entries()) {
-        console.log('the index', index)
-        const {id} = person;
-        const newPerson = await prisma.person.update({
-          where: { 
-            id
-          },
-          data: { 
-            order: index+1
-           },
-        });
-        updatedPeople.push(newPerson)
-      }
+      const { people } = req.body;
+    
+      const updatedPeople = prisma.$transaction(
+        people.map(async (person, index) => {
+          const { id } = person;
+          const updatedPerson = await prisma.person.update({
+            where: { id },
+            data: { order: index + 1 },
+          });
+          return updatedPerson;
+        })
+      );
+    
       return res.status(200).json(updatedPeople);
-      }
-     catch (error) {
-      console.error(`An error occurred while trying to edit people: ${error}`);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Something went wrong" });
     }
+    
   }
